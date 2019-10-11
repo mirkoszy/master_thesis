599c13eef5e2414fc55fdc7a56a69580a492aa17
Miroslaw Szewczyk <mszewczyk@Miroslaws-MacBook-Pro.local>
2019-10-11 19:53:35 +0200

    fix in prepare vm script

--- prepare_vm.sh
+++ prepare_vm.sh
@@ -4,12 +4,14 @@ sudo apt update
 
 sudo apt install -y python3 python3-pip python-pip htop stress-ng wget openssl build-essential libssl-dev git iperf libusb-1.0-0 libasound2 libcurl4-openssl-dev
 
+cd /users/mirkoszy/
+
 git clone https://github.com/wg/wrk.git wrk
 
 cd wrk && make && sudo cp wrk /usr/local/bin
 
-cd ~/ && git clone https://github.com/jeffhammond/STREAM.git
+cd /users/mirkoszy/ && git clone https://github.com/jeffhammond/STREAM.git
 
 cd STREAM && gcc -O2 -fopenmp stream.c -o ../stream
 
-cd ~/ wget https://www.passmark.com/downloads/bitlinux.tar.gz && tar -xvf bitlinux.tar.gz
+cd /users/mirkoszy/ wget https://www.passmark.com/downloads/bitlinux.tar.gz && tar -xvf bitlinux.tar.gz
