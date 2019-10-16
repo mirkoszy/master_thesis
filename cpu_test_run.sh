d2aca11b29d073dd7cc5dbadc149dd3f975e3e66
mirkoszy <szewczyk.mw@gmail.com>
2019-10-16 09:49:58 -0600

    added kcbench and modified cpu test

--- cpu_test_run.sh
+++ cpu_test_run.sh
@@ -1,38 +1,13 @@
 OUTPUT=~/master_thesis/outputs
 RESULT_FILE="${OUTPUT}/result.out"
-cipher_commands="md5 sha512 aes-256-cbc rsa2048 dsa2048"
-for test in ${cipher_commands}; do
+test="rsa2048"
+for i in {1..34}; do
     echo "Running openssl speed ${test} test"
     openssl speed -multi 16 "${test}" 2>&1 | tee "${OUTPUT}/${test}-output.txt"
 
-    case "${test}" in
-      # Parse asymmetric encryption output.
-      rsa2048|dsa2048)
-        awk -v test_case_id="${test}" 'match($1$2, test_case_id) \
-            {printf("%s-sign pass %s sign/s\n", test_case_id, $(NF-1)); \
-            printf("%s-verify pass %s verify/s\n", test_case_id, $NF)}' \
-            "${OUTPUT}/${test}-output.txt" | tee -a "${RESULT_FILE}"
-        ;;
-      # Parse symmetric encryption output.
-      (aes-256-cbc)
-        awk -v test_case_id="${test}" \
-            '/^Doing/ {printf("%s-%s pass %d bytes/s\n", test_case_id, $7, $7*$10/3)}' \
-            "${OUTPUT}/${test}-output.txt" | tee -a "${RESULT_FILE}"
-        ;;
-      *)
-        awk -v test_case_id="${test}" \
-            '/^Doing/ {printf("%s-%s pass %d bytes/s\n", test_case_id, $6, $6*$9/3)}' \
-            "${OUTPUT}/${test}-output.txt" | tee -a "${RESULT_FILE}"
-        ;;
-    esac
+    awk -v test_case_id="${test}" 'match($1$2, test_case_id) \
+      {printf("%s-sign pass %s sign/s\n", test_case_id, $(NF-1)); \
+      printf("%s-verify pass %s verify/s\n", test_case_id, $NF)}' \
+      "${OUTPUT}/${test}-output.txt" | tee -a "${RESULT_FILE}"
+    ./kcbench >> kcbench.log 
 done
-
-stress-ng --cpu 16 --cpu-method fft -t 60 --metrics-brief 2>  "${OUTPUT}/fft-output.txt"
-stress-ng --cpu 16 --cpu-method pi -t 60 --metrics-brief 2> "${OUTPUT}/pi-output.txt"
-stress-ng --cpu 16 --matrix 16 -t 60 --matrix-method hadamard --metrics-brief 2> "${OUTPUT}/matrix-output.txt"
-
-echo "FFT BOGOS/s: $(cat ${OUTPUT}/fft-output.txt | grep cpu | awk '{print $9}')" >> "${RESULT_FILE}"
-echo "PI BOGOS/s: $(cat ${OUTPUT}/pi-output.txt | grep cpu | awk '{print $9}')" >>  "${RESULT_FILE}"
-echo "MATRIX BOGOS/s: $(cat ${OUTPUT}/matrix-output.txt | grep cpu | awk '{print $9}')" >> "${RESULT_FILE}"
-
-rm ${OUTPUT}/*.txt
