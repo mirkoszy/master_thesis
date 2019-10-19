bf0704e355595bab0aaf9b6910b0e2271a3bdefd
mirkoszy <szewczyk.mw@gmail.com>
2019-10-19 18:59:00 +0200

    Update cpu_test_run.sh

--- cpu_test_run.sh
+++ cpu_test_run.sh
@@ -2,7 +2,7 @@ OUTPUT=/master_thesis/outputs
 RESULT_FILE="${OUTPUT}/result.out"
 test="rsa2048"
 for i in {1..34}; do
-    openssl speed -multi 4 "${test}" 2>&1 | tee "${OUTPUT}/${test}-output.txt";
+    openssl speed -multi 8 "${test}" 2>&1 | tee "${OUTPUT}/${test}-output.txt";
 
     awk -v test_case_id="${test}" 'match($1$2, test_case_id) \
       {printf("%s-sign pass %s sign/s\n", test_case_id, $(NF-1)); \
